bdc6c7506ce39f653b7ab4736947a2b19382d071
Juan Ramon Montilla Sanchez <ing.juan.montilla@gmail.com>
2024-02-12 16:18:13 -0400

    primera version

---
+++ src/app/contador.Component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-Contador',
+    standalone: true, 
+    imports: [RouterOutlet],
+    template: '  mundo', 
+  })
+  export class contadorComponent {
+    title = 'Practica';
+  }
+  
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { contadorComponent } from './app/contador.Component';
+ 
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+ 
